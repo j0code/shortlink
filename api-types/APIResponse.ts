dcbe86efb1b4215a6b04a8e24d7209141573a7bf
j0code <jonaslp759@gmail.com>
2026-09-16 20:50:51 +0200

    feat: implement APIResponse schema

--- api-types/APIResponse.ts
+++ api-types/APIResponse.ts
@@ -1,4 +1,4 @@
-import type { ErrorStatus, SuccessStatus } from "./base.ts"
+import { errorStatusSchema, successStatusSchema, type ErrorStatus, type SuccessStatus } from "./base.ts"
 import * as v from "@valibot/valibot"
 
 export type APIResponse<T = unknown> = {
@@ -18,4 +18,56 @@ export function success<T>(result: T, status: SuccessStatus = 200): APIResponse<
 
 export function error(message: string, status: ErrorStatus = 400, details?: unknown): APIResponse<never> {
 	return { success: false, status, error: message, details }
+}
+
+const apiResponseSchema = v.variant("success", [
+	v.object({
+		success: v.literal(true),
+		status: successStatusSchema,
+		result: v.unknown()
+	}),
+	v.object({
+		success: v.literal(false),
+		status: errorStatusSchema,
+		error: v.string(),
+		details: v.optional(v.unknown())
+	})
+])
+
+type ObjectSchema<
+TEntries extends v.ObjectEntries = v.ObjectEntries,
+TMessage extends v.ErrorMessage<v.ObjectIssue> | undefined = v.ErrorMessage<v.ObjectIssue> | undefined
+> = v.ObjectSchema<TEntries, TMessage>
+
+type ParseResult<TSchema extends ObjectSchema> = {
+	success: true,
+	output: APIResponse<v.InferOutput<TSchema>>
+} | {
+	success: false,
+	issues: [v.InferIssue<TSchema>, ...v.InferIssue<TSchema>[]]
+}
+
+export function parseAPIResponse<
+TEntries extends v.ObjectEntries,
+TMessage extends v.ErrorMessage<v.ObjectIssue> | undefined
+>(dataSchema: ObjectSchema<TEntries, TMessage>, res: unknown): ParseResult<typeof dataSchema> {
+	const result = v.safeParse(apiResponseSchema, res)
+
+	if (!result.success) {
+		return { success: false, issues: result.issues }
+	}
+
+	const output = result.output
+	
+	if (!output.success) {
+		return { success: true, output }
+	}
+
+	const { success, output: dataOutput, issues } = v.safeParse(dataSchema, output.result)
+
+	if (!success) {
+		return { success: false, issues}
+	}
+
+	return { success: true, output: {...output, result: dataOutput} }
 }
--- api-types/base.ts
+++ api-types/base.ts
@@ -1,12 +1,24 @@
 import * as v from "@valibot/valibot"
 
 export type Method = "GET" | "POST" | "DELETE"
-export type SuccessStatus = 200
-export type ErrorStatus   = 400 | 401 | 403 |404 | 500
-
 export type Params = Record<string, string>
 
 export const isoDate = v.pipe(
 	v.string(),
 	v.isoTimestamp(),
 )
+
+export const successStatusSchema = v.union([
+	v.literal(200)
+])
+
+export const errorStatusSchema = v.union([
+	v.literal(400),
+	v.literal(401),
+	v.literal(403),
+	v.literal(404),
+	v.literal(500),
+])
+
+export type SuccessStatus = v.InferOutput<typeof successStatusSchema>
+export type ErrorStatus = v.InferOutput<typeof errorStatusSchema>
