744b77636dbe7ba3d8b36c1d77d8d18183078e49
j0code <jonaslp759@gmail.com>
2026-09-17 20:57:55 +0200

    fix: homepage renders shortlinks with /api prefix

--- client/src/home.ts
+++ client/src/home.ts
@@ -25,7 +25,7 @@ createForm.addEventListener("submit", async event => {
 		return
 	}
 
-	const shortlinkUrl = new URL(result.result.id, api.baseUrl).href
+	const shortlinkUrl = new URL(`/${result.result.id}`, api.baseUrl).href
 	const shortlinkOutput   = createForm.elements.namedItem("shortlink")   as HTMLInputElement
 	const shortlinkIdOutput = createForm.elements.namedItem("shortlinkId") as HTMLInputElement
 	const linkButton = shortlinkOutput.nextElementSibling!.children[0] as HTMLAnchorElement
